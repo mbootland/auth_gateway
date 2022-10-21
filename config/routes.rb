fc255898b6ebcac71fce76fabdc0f68f17135bc5
mbootland <matthew.bootland@gmail.com>
2022-10-21 18:13:50 +0900

    fixing Rails 7 turbostream issues and devise is now going through proper controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,17 @@
 Rails.application.routes.draw do
-  devise_for :users
+
+  # Custom devise routes for overriding create method
+  devise_for :users, controllers: {
+    confirmations: 'users/confirmations',
+    omniauth_callbacks: 'users/omniauth_callbacks',
+    passwords: 'users/passwords',
+    registrations: 'users/registrations',
+    sessions: 'users/sessions',
+    unlocks: 'users/unlocks'
+  }
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "home#index"
 end
